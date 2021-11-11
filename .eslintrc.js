d004a2fd6f73386695f9e69238570ec569796bda
mamoru igamoto <stream99990@yahoo.co.jp>
2021-11-12 00:06:13 +0900

    update: セミコロンは無しが好みなので変更しました

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
 	extends: ["next", "next/core-web-vitals"],
 	rules: {
-		semi: "error",
+		semi: "never",
 		"import/prefer-default-export": "off",
 		"newline-before-return": "error",
 		"no-console": "warn",
