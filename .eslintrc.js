54425e9f6bdd4c422dfa08963da6f9b6351d5397
mamoru igamoto <stream99990@yahoo.co.jp>
2021-11-12 00:17:13 +0900

    update: ESLintとPrettierがバッティングしないように連携させる

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-	extends: ["next", "next/core-web-vitals"],
+	extends: ['next', 'next/core-web-vitals', 'prettier'],
 	rules: {
-		semi: "never",
-		"import/prefer-default-export": "off",
-		"newline-before-return": "error",
-		"no-console": "warn",
-		"no-var": "error",
+		semi: 'never',
+		'import/prefer-default-export': 'off',
+		'newline-before-return': 'error',
+		'no-console': 'warn',
+		'no-var': 'error',
 	},
 };
