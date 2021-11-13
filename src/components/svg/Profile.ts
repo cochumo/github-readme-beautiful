9edbcc161709aaa0615d5402fcae243999fac243
mamoru igamoto <stream99990@yahoo.co.jp>
2021-11-13 21:13:44 +0900

    update: プロフィールを修正しました

--- src/components/svg/Profile.ts
+++ src/components/svg/Profile.ts
@@ -1,4 +1,16 @@
-const Profile = () => {
+import dayjs from 'dayjs'
+
+const Profile = (
+	name: string = 'unknow',
+	age: string = 'unknow',
+	local: string = 'unknow',
+	type: string = 'unknow',
+	birthDate?: string
+) => {
+	if (birthDate && dayjs(birthDate).isValid()) {
+		let birthday = dayjs(birthDate)
+		age = String(dayjs().diff(birthday, 'year'))
+	}
 	return `
 		<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="846" height="423" viewBox="0 0 846 423">
 			<defs>
@@ -33,10 +45,12 @@ const Profile = () => {
 				<text id="Age" transform="translate(134 198)" fill="#83a5c0" font-size="18" font-family="Poppins-Medium, Poppins" font-weight="500"><tspan x="0" y="0">Age</tspan></text>
 				<text id="Local" transform="translate(134 239)" fill="#83a5c0" font-size="18" font-family="Poppins-Medium, Poppins" font-weight="500"><tspan x="0" y="0">Local</tspan></text>
 				<text id="Type" transform="translate(134 280)" fill="#83a5c0" font-size="18" font-family="Poppins-Medium, Poppins" font-weight="500"><tspan x="0" y="0">Type</tspan></text>
-				<text id="cochumo" transform="translate(269 157)" fill="#bfbfbf" font-size="18" font-family="Poppins-Medium, Poppins" font-weight="500"><tspan x="0" y="0">cochumo</tspan></text>
-				<text id="_29" data-name="29" transform="translate(269 198)" fill="#bfbfbf" font-size="18" font-family="Poppins-Medium, Poppins" font-weight="500"><tspan x="0" y="0">29</tspan></text>
-				<text id="Japan" transform="translate(269 239)" fill="#bfbfbf" font-size="18" font-family="Poppins-Medium, Poppins" font-weight="500"><tspan x="0" y="0">Japan</tspan></text>
-				<text id="Front-end_engineer" data-name="Front-end engineer" transform="translate(268 280)" fill="#bfbfbf" font-size="18" font-family="Poppins-Medium, Poppins" font-weight="500"><tspan x="0" y="0">Front-end engineer</tspan></text>
+				<text id="NameText" data-name="${name}" transform="translate(269 157)" fill="#bfbfbf" font-size="18" font-family="Poppins-Medium, Poppins" font-weight="500"><tspan x="0" y="0">${name}</tspan></text>
+				<text id="AgeText" data-name="${age}" transform="translate(269 198)" fill="#bfbfbf" font-size="18" font-family="Poppins-Medium, Poppins" font-weight="500"><tspan x="0" y="0">${age}</tspan></text>
+				<text id="LocalText" data-name="${local}" transform="translate(269 239)" fill="#bfbfbf" font-size="18" font-family="Poppins-Medium, Poppins" font-weight="500"><tspan x="0" y="0">${
+		local[0].toUpperCase() + local.slice(1)
+	}</tspan></text>
+				<text id="TypeText" data-name="${type}" transform="translate(268 280)" fill="#bfbfbf" font-size="18" font-family="Poppins-Medium, Poppins" font-weight="500"><tspan x="0" y="0">${type}</tspan></text>
 			</g>
 		</svg>
 	`
--- src/pages/api/profile.ts
+++ src/pages/api/profile.ts
@@ -2,9 +2,17 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import Profile from '../../components/svg/Profile'
 
 export default function handler(
-	_req: NextApiRequest,
+	req: NextApiRequest,
 	res: NextApiResponse<string>
 ) {
+	let {
+		query: { name, age, local, type, birthDate },
+	} = req
+	name = Array.isArray(name) ? name[0] : name
+	age = Array.isArray(age) ? age[0] : age
+	local = Array.isArray(local) ? local[0] : local
+	type = Array.isArray(type) ? type[0] : type
+	birthDate = Array.isArray(birthDate) ? birthDate[0] : birthDate
 	res.setHeader('Content-Type', 'image/svg+xml')
-	res.status(200).send(Profile())
+	res.status(200).send(Profile(name, age, local, type, birthDate))
 }
