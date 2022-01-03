30d6b8d05834ab73587a1d836e0f8ac4986f0966
mamoru igamoto <stream99990@yahoo.co.jp>
2022-01-03 14:35:21 +0900

    update: コンポーネントの名前を変更しました

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Index from '../components/pages/Index'
+import Index from '../components/pages/IndexPage'
 
 const Home: NextPage = () => {
 	return (
